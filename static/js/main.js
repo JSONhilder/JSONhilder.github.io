5b5fd27a00c6e7efbf4d31248e97f9d3c727b338
Jason Hilder <jhilder95@gmail.com>
2023-06-16 20:56:33 +0200

    Updates.

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() { var e = window.location.pathname.split("/").filter(e => e); e.length > 1 && "entries" === e[0] && (document.querySelector("footer").style.cssText = "position:relative;margin-top:25px;margin-bottom:15px;"), localStorage.getItem("light-mode") || (document.body.classList.toggle("dark"), document.getElementById("theme-switch").innerHTML = "Light Mode") }, !1), document.getElementById("theme-switch").addEventListener("click", function() { document.body.classList.toggle("dark"), document.body.classList.contains("dark") ? (document.getElementById("theme-switch").innerHTML = "Light Mode", localStorage.removeItem("light-mode")) : (document.getElementById("theme-switch").innerHTML = "Dark Mode", localStorage.setItem("light-mode", !0)) });
+document.addEventListener(
+    "DOMContentLoaded",
+    function() {
+        var e = window.location.pathname.split("/").filter((e) => e);
+        e.length > 1 && "entries" === e[0] && (document.querySelector("footer").style.cssText = "position:relative;margin-top:25px;margin-bottom:15px;")
+    },
+    !1
+),
+    document.getElementById("theme-switch").addEventListener("click", function() {
+        document.body.classList.toggle("dark");
+        document.body.classList.contains("dark") ?
+            (document.getElementById("theme-switch").innerHTML = "Light Mode")
+            : (document.getElementById("theme-switch").innerHTML = "Dark Mode");
+    });
